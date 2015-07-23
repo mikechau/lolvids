b1ed417c9b97e254bf27fa83c22a5df7f216d5ed
Mike Chau <mike@chau.io>
2015-07-23 18:34:44 -0500

    react: add additional TopNavBar tests

--- react/test/components/TopNavBar.test.js
+++ react/test/components/TopNavBar.test.js
@@ -2,6 +2,10 @@ var React = require('react');
 var ReactTestUtils = require('react/lib/ReactTestUtils');
 var TopNavBar = require('app/components/TopNavBar');
 
+var eventNoop = {
+  preventDefault: function() {}
+};
+
 describe('TopNavBar', function() {
   describe('on initial render', function() {
     it('renders: a nav element', function() {
@@ -12,5 +16,33 @@ describe('TopNavBar', function() {
       expect(component.type).to.equal('nav');
       expect(component.props.className).to.equal('navbar navbar-default navbar-static-top');
     });
+  });
+
+  describe('component handlers', function() {
+    it('#handleEndlessModeClick', function(done) {
+      var callback = function() {
+        done();
+      };
+
+      var component = ReactTestUtils.renderIntoDocument(
+        <TopNavBar onEndlessModeClick={callback} />
+      );
+
+      component.handleEndlessModeClick(eventNoop);
+    });
+  });
+
+  describe('#handleCollapseNavClick', function(done) {
+    var component = ReactTestUtils.renderIntoDocument(
+      <TopNavBar />
+    );
+
+    component.handleCollapseNavClick();
+
+    expect(component.state.showCollapseNav, 'showCollapseNav state not true').to.be.true;
+
+    component.handleCollapseNavClick();
+
+    expect(component.state.showCollapseNav, 'showCollapseNav state not false').to.be.false;
   });
 });
